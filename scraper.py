c0963198789d65cf7372b6a5e1e1b891b8e1eeef
Yijie Zhu <zhuy80@cardiff.ac.uk>
2020-04-29 16:27:35 +0800

    the original project without framework

---
+++ scraper.py
@@ -0,0 +1,87 @@
+from bs4 import BeautifulSoup
+import requests
+import pymysql
+import threading
+import time
+
+# define the function that used for inserting the data into the database
+def getCaseDetails(case_year, division_code, case_id):
+
+    try:  
+
+        item = str(case_year + division_code + case_id)
+    
+        url = 'https://courtlink.lexisnexis.com/cookcounty/FindDock.aspx?NCase='+ item + '&SearchType=0&Database=4&case_no=&PLtype=1&sname=&CDate='
+        
+        # use the ip proxy from crawlera
+        response = requests.get(
+            url,
+            proxies={
+                "http": "http://ff42eacc69c9495bb5cb7df4d7177a6a:@proxy.crawlera.com:8010/",
+            },
+        )
+       
+        html = response.content                
+        
+        # use the parser with lxml
+        bs = BeautifulSoup(html,"lxml")
+
+        # case number
+        case_number = bs.find_all('span',attrs={'id':'lblBottom'})[0].string
+
+        # plaintiff
+        tr = bs.find_all('tr',limit=5)[4]
+        plaintiff = tr.find_all('td')[0].string
+
+        # defendant
+        defendant_ori = []
+        tbd = bs.find_all('tbody',limit=3)[1]
+        tr2s = tbd.find_all('tr')
+        list_tr2s = list(tr2s)
+
+        for index in range(len(list_tr2s)):
+            if list_tr2s[index].find_all('td')[0].string == 'Defendant(s)': 
+                defendant_ori.append(list_tr2s[index+1].find_all('td')[0].string)
+                if (len(list_tr2s[index+2:])) != 0:
+                    for i in (list_tr2s[index+2:]):
+                        if i.find_all('td')[0].string != None:
+                            defendant_ori.append(str(i.find_all('td')[0].string))
+                    # for index2 in range(len(list_tr2s[index+1:])):
+                    
+                        # print(list_tr2s[index2].find_all('td')[0].string)           
+        defendant = str(defendant_ori)
+
+        # filling date
+        tr3 = bs.find_all('tr',limit=1)[0]
+        td3 = tr3.find_all('td')[0].string
+        filling_date = td3[14:]
+        
+        # define the databse
+        conn = pymysql.connect(host='localhost',user='root',password='82829576',database='scraper',port=3306)
+
+        cursor = conn.cursor()
+        
+        # fill with the sql 
+        sql = """ 
+        insert into result(case_number,plaintiff,defendant,filling_date) values(%s,%s,%s,%s)
+        """
+
+        cursor.execute(sql,(case_number,plaintiff,defendant,filling_date))
+
+        conn.commit()
+
+        conn.close()
+
+    except:
+        
+        print('this page does not exist' )
+
+# define the automated function
+def multi_thread():
+    for i in range(300,400):
+        # asynchronous process:
+        task = threading.Thread(target=getCaseDetails, args=('1998','D',str(i).zfill(6)))
+        task.start()  
+
+# run the function to scrape the date from the task page
+multi_thread()
